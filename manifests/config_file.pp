5786bb492cb2483eca4140ee3127729aaf6a0b04
Clayton O'Neill <clayton.oneill@twcable.com>
2015-08-24 19:20:55 +0000

    Reworked and renamed to support multiple services.
    Renamed everything to be under the os_docker name space and reworked to
    allow adding additional OpenStack services.  Removed all virtualenv
    support.
    
    Change-Id: I9da52a8d48b1a3e09b921ba3503e957618dca2af

--- manifests/config_file.pp
+++ manifests/config_file.pp
@@ -6,6 +6,26 @@
 #
 # === Parameters
 #
+# [*config_dir*]
+#  (required) Name of the base directory for all config files for the project.
+#  It is expected that the project will set the default for this.
+#
+# [*owner*]
+#  (required) Owner of the config file.  It is expected that the project will
+#  set the default for this.
+#
+# [*group*]
+#  (required) Group of the config file.  It is expected that the project will
+#  set the default for this.
+#
+# [*mode*]
+#  (required) Mode of the config file.  Defaults to "0640".
+#
+# [*source_dir*]
+#  (required) Puppet URL to use as the base for all source parameters.  The
+#  filename will be appended to this to find the file.  It is expected that the
+#  project will set the default for this.
+#
 # [*file*]
 #  (optional) The name of the file to manage.  The directory should not be
 #  specified, since it will be assumed to be relative to /etc/designate.
@@ -21,40 +41,31 @@
 #  to copy example config files from virtualenv git repos, but it can be used
 #  to copy config files from anywhere.  Defaults to undef.
 #
-# [*link_from_dir*]
-#  (optional) If specified, then link the config file given into the directory
-#  given.  This is used to link the config files in /etc/designate into a
-#  virtualenv environment.  Defines to undef.
-#
 # [*replace*]
 #  (optional) If true, then files in source_dir will replace any config files
 #  in /etc/designate when they change.  If false, then existing files will be
 #  unmodified.  Defaults to false.
 #
-define designate_ext::config_file(
+define os_docker::config_file(
+  $config_dir,
+  $owner,
+  $group,
+  $source_dir,
   $file          = $name,
+  $mode          = '0640',
   $ensure        = file,
-  $source_dir    = 'puppet:///modules/designate_ext/config',
-  $link_from_dir = undef,
   $replace       = false,
 ) {
   if $source_dir {
     $source = ["${source_dir}/${file}", "${source_dir}/${file}.sample"]
   }
-  file { "/etc/designate/${file}":
+  file { "${config_dir}/${file}":
     ensure  => $ensure,
-    owner   => 'designate',
-    group   => 'designate',
-    mode    => '0640',
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
     source  => $source,
     replace => $replace,
   }
 
-  if $link_from_dir {
-    file { "${link_from_dir}/${file}":
-      ensure => 'link',
-      force  => true,
-      target => "/etc/designate/${file}",
-    }
-  }
 }
