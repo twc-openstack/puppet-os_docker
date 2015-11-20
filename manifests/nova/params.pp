fb691d0872606aebcded095782ea8469cdd6eadd
Clayton O'Neill <clayton.oneill@twcable.com>
2015-11-20 13:45:27 +0000

    Add Nova control node services support
    This doesn't include support for Nova compute (yet)
    
    Change-Id: Idca0f020ea4cd31c15823afa0bcaa863967bc848

---
+++ manifests/nova/params.pp
@@ -0,0 +1,26 @@
+class os_docker::nova::params {
+  include ::nova::params
+
+  $managed_dirs = [
+    '/etc/nova',
+    '/etc/nova/rootwrap.d',
+    '/var/lib/nova',
+    '/var/lock/nova',
+    '/var/log/nova',
+  ]
+
+  # By default with packages the file contents aren't managed, so the setting
+  # for replace doesn't matter.  However with docker, the files from the
+  # source checkout are copied by default.  We'll want to replace most of the
+  # config files if they've changed, and leave users the option to override via
+  # parameters into the os_docker::nova class.
+  $config_files  = {
+    '/etc/nova/nova.conf'                           => { replace => false },
+    '/etc/nova/api-paste.ini'                       => { replace => false },
+    '/etc/nova/logging.conf'                        => { replace => true },
+    '/etc/nova/policy.json'                         => { replace => true },
+    '/etc/nova/rootwrap.conf'                       => { replace => true },
+    '/etc/nova/rootwrap.d/api-metadata.filters'     => { replace => true },
+    '/etc/nova/rootwrap.d/compute.filters'          => { replace => true },
+  }
+}
