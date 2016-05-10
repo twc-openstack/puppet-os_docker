9a3768a8660656a15a4dd365b3cdc156688cd0b7
Adam Vinsh <adam.vinsh@twcable.com>
2016-05-10 16:53:06 +0000

    WIP puppet-swift docker mgmt
    Issue: CIRRUS-4512
    Change-Id: I20c1ccf38f0254107a093f88b10415b8c234ff65

---
+++ manifests/swift/params.pp
@@ -0,0 +1,18 @@
+class os_docker::swift::params {
+  include ::swift::params
+
+  $managed_dirs = [
+    '/var/log/swift',
+    '/etc/swift',
+  ]
+
+  # By default with packages the file contents aren't managed, so the setting
+  # for replace doesn't matter.  However with docker, the files from the
+  # source checkout are copied by default.  We'll want to replace most of the
+  # config files if they've changed, and leave users the option to override via
+  # parameters into the os_docker::keystone class.
+  $config_files  = {
+#    '/etc/keystone/default_catalog.templates'  => { replace => true },
+#    '/etc/keystone/keystone.conf'              => { replace => false },
+  }
+}
