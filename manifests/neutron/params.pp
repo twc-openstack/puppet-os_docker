93044c1935ef72d3fed90114ddc388a90c85a91f
Clayton O'Neill <clayton.oneill@twcable.com>
2016-05-31 12:04:27 +0000

    Add Neutron support
    Change-Id: I04c32ab58e6342c930252d2182d6bd8983144cfa

---
+++ manifests/neutron/params.pp
@@ -0,0 +1,57 @@
+class os_docker::neutron::params {
+  include ::neutron::params
+
+  $managed_dirs = [
+    '/etc/neutron',
+    '/etc/neutron/plugins',
+    '/etc/neutron/plugins/ml2',
+    '/etc/neutron/rootwrap.d',
+    '/var/lib/neutron',
+    '/var/lock/neutron',
+    '/var/log/neutron',
+  ]
+
+  # By default with packages the file contents aren't managed, so the setting
+  # for replace doesn't matter.  However with docker, the files from the
+  # source checkout are copied by default.  We'll want to replace most of the
+  # config files if they've changed, and leave users the option to override via
+  # parameters into the os_docker::glance class.
+  $config_files  = {
+    '/etc/neutron/api-paste.ini'                          => { replace => true },
+    '/etc/neutron/dhcp_agent.ini'                         => { replace => true },
+    '/etc/neutron/l3_agent.ini'                           => { replace => true },
+    '/etc/neutron/metadata_agent.ini'                     => { replace => true },
+    '/etc/neutron/metering_agent.ini'                     => { replace => true },
+    '/etc/neutron/neutron.conf'                           => { replace => true },
+    '/etc/neutron/plugins/ml2/linuxbridge_agent.ini'      => { replace => true },
+    '/etc/neutron/plugins/ml2/ml2_conf_brocade_fi_ni.ini' => { replace => false },
+    '/etc/neutron/plugins/ml2/ml2_conf_brocade.ini'       => { replace => false },
+    '/etc/neutron/plugins/ml2/ml2_conf_fslsdn.ini'        => { replace => false },
+    '/etc/neutron/plugins/ml2/ml2_conf.ini'               => { replace => true },
+    '/etc/neutron/plugins/ml2/ml2_conf_ofa.ini'           => { replace => false },
+    '/etc/neutron/plugins/ml2/ml2_conf_sriov.ini'         => { replace => false },
+    '/etc/neutron/plugins/ml2/openvswitch_agent.ini'      => { replace => true },
+    '/etc/neutron/plugins/ml2/sriov_agent.ini'            => { replace => false },
+    '/etc/neutron/policy.json'                            => { replace => false },
+    '/etc/neutron/rootwrap.conf'                          => { replace => false },
+    '/etc/neutron/rootwrap.d/debug.filters'               => { replace => false },
+    '/etc/neutron/rootwrap.d/dhcp.filters'                => { replace => false },
+    '/etc/neutron/rootwrap.d/dibbler.filters'             => { replace => false },
+    '/etc/neutron/rootwrap.d/ebtables.filters'            => { replace => false },
+    '/etc/neutron/rootwrap.d/ipset-firewall.filters'      => { replace => false },
+    '/etc/neutron/rootwrap.d/iptables-firewall.filters'   => { replace => false },
+    '/etc/neutron/rootwrap.d/l3.filters'                  => { replace => false },
+    '/etc/neutron/rootwrap.d/linuxbridge-plugin.filters'  => { replace => false },
+    '/etc/neutron/rootwrap.d/openvswitch-plugin.filters'  => { replace => false },
+  }
+
+  $volumes = [
+    '/run/openvswitch:/run/openvswitch',
+    '/lib/modules:/lib/modules:ro',
+    '/etc/neutron:/etc/neutron:ro',
+    '/var/log/neutron:/var/log/neutron',
+    '/var/lib/neutron:/var/lib/neutron',
+    '/run/lock/neutron:/run/lock/neutron',
+    '/run/neutron:/run/neutron',
+  ]
+}
---
+++ manifests/neutron/server.pp
@@ -0,0 +1,73 @@
+# == Class: os_docker::neutron::server
+#
+# This class handles any docker related changes needed for this service.
+# Currently this includes creating the docker container and the startup script
+# to go with it.
+#
+# === Parameters
+#
+# [*manage_service*] (optional) Whether or not to manage the docker container
+# for this service.  Default: true
+#
+# [*run_override*] (optional) Hash of additional parameters to use when
+# creating the Docker::Run resource.  Default: none
+#
+# [*active_image_name*] (optional) The name of the docker image to use for the
+# service container.  Defaults to the active container set via the main
+# os_docker::neutron class.
+#
+# [*active_image_tag*] (optional) The tag of the docker image to use for the
+# service container.  Defaults to the active container set via the main
+# os_docker::neutron class.
+#
+# [*groups*] (optional) Groups that the neutron user inside the container should
+# be a member of.
+#
+# [*extra_volumes*] (optional) Extra docker volumes to mount inside the
+# container.  This will be passed directly to docker in addition tho the normal
+# volumes
+#
+# [*before_start*] (optional) Shell script part that will be run before the
+# service is started.  This can be used to ensure neutron-ovs-cleanup has
+# already run before neutron-server is started.
+#
+class os_docker::neutron::server(
+  $manage_service    = true,
+  $run_override      = {},
+  $active_image_name = $::os_docker::neutron::active_image_name,
+  $active_image_tag  = $::os_docker::neutron::active_image_tag,
+  $extra_volumes     = [],
+  $before_start      = false,
+){
+  include ::neutron::server::params
+
+  if $active_image_name {
+    if $manage_service {
+      $default_params = {
+        image            => "${active_image_name}:${active_image_tag}",
+        command          => '/usr/bin/neutron-server',
+        net              => 'host',
+        privileged       => false,
+        service_prefix   => '',
+        manage_service   => false,
+        extra_parameters => ['--restart=always'],
+        volumes          => concat($os_docker::neutron::params::volumes, $extra_volumes),
+        tag              => ['neutron-docker'],
+        before_start     => $before_start,
+      }
+
+      $server_resource = merge($default_params, $run_override)
+      create_resources('::docker::run', { 'neutron-server' => $server_resource } )
+    }
+
+    docker::command { '/usr/bin/neutron-server':
+      command    => '/usr/bin/neutron-server',
+      image      => "${active_image_name}:${active_image_tag}",
+      net        => 'host',
+      env        => $environment,
+      privileged => true,
+      volumes          => concat($os_docker::neutron::params::volumes, $extra_volumes),
+      tag        => ['neutron-docker'],
+    }
+  }
+}
