947f1e1787c4de698ae65d4d9574db95cd17734b
Adam Vinsh <adam.vinsh@charter.com>
2016-08-19 21:25:42 +0000

    Support ironic services in a docker container
    Run ironic-api, ironic-conductor, ironic-dbsync
    in a container.
    - mitaka release
    
    Change-Id: Ic192553f5cdb3752a3f477825679f367eea7104d

---
+++ manifests/ironic/conductor.pp
@@ -0,0 +1,64 @@
+# == Class: os_docker::ironic::conductor
+#
+# This class handles any docker related changes needed for this service.
+# Currently this includes creating the docker container and the startup script
+# to go with it.
+#
+# === Parameters
+#
+# [*manage_service*] (optional) Whether or not to manage the docker container
+# for this service.  Default: true
+#
+# [*run_override*] (optional) Hash of additional parameters to use when
+# creating the Docker::Run resource.  Default: none
+#
+# [*active_image_name*] (optional) The name of the docker image to use for the
+# service container.  Defaults to the active container set via the main
+# os_docker::ironic class.
+#
+# [*active_image_tag*] (optional) The tag of the docker image to use for the
+# service container.  Defaults to the active container set via the main
+# os_docker::ironic class.
+#
+class os_docker::ironic::conductor(
+  $manage_service    = true,
+  $run_override      = {},
+  $active_image_name = $::os_docker::ironic::active_image_name,
+  $active_image_tag  = $::os_docker::ironic::active_image_tag,
+){
+  include ::os_docker::ironic
+
+  if $active_image_name {
+      $vols_default = [
+          '/etc/ironic:/etc/ironic:ro',
+          '/var/log/ironic:/var/log/ironic',
+          '/var/lock/ironic:/var/lock/ironic',
+          '/var/lib/ironic:/var/lib/ironic',
+          '/tftpboot:/tftpboot',
+      ]
+
+    if $manage_service {
+      $default_params = {
+        image            => "${active_image_name}:${active_image_tag}",
+        command          => '/usr/bin/ironic-conductor',
+        net              => 'host',
+        volumes          => $vols_default,
+        tag              => ['ironic-docker'],
+        service_prefix   => '',
+        manage_service   => false,
+        extra_parameters => ['--restart=always'],
+      }
+
+      $conductor_resource = merge($default_params, $run_override)
+      create_resources('::docker::run', { 'ironic-conductor' => $conductor_resource } )
+    }
+
+    docker::command { '/usr/bin/ironic-conductor':
+      command    => '/usr/bin/ironic-conductor',
+      image      => "${active_image_name}:${active_image_tag}",
+      net        => 'host',
+      volumes    => $vols_default,
+      tag        => ['ironic-docker'],
+    }
+  }
+}
