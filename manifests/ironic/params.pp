d649ac9f17842abe8e5f582503845de788e64e5c
Adam Vinsh <adam.vinsh@charter.com>
2016-08-19 21:25:42 +0000

    dockerize ironic
    Run ironic-api, ironic-conductor, ironic-dbsync
    in a container.
    
    Change-Id: Ic192553f5cdb3752a3f477825679f367eea7104d

---
+++ manifests/ironic/params.pp
@@ -0,0 +1,24 @@
+class os_docker::ironic::params {
+  include ::ironic::params
+
+  $managed_dirs = [
+    '/etc/ironic',
+    '/etc/ironic-inspector',
+    '/etc/ironic-inspector/rootwrap.d',
+    '/var/lib/ironic',
+    '/var/lock/ironic',
+    '/var/log/ironic',
+  ]
+
+  # By default with packages the file contents aren't managed, so the setting
+  # for replace doesn't matter.  However with docker, the files from the
+  # source checkout are copied by default.  We'll want to replace most of the
+  # config files if they've changed, and leave users the option to override via
+  # parameters into the os_docker::ironic class.
+  $config_files  = {
+    '/etc/ironic/ironic.conf'                                             => { replace => false },
+    '/etc/ironic-inspector/inspector.conf'                                => { replace => false },
+    '/etc/ironic-inspector/rootwrap.conf'                                 => { replace => true },
+    '/etc/ironic-inspector/rootwrap.d/ironic-inspector-firewall.filters'  => { replace => true },
+  }
+}
