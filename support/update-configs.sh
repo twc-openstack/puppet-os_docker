dbaa2bf4b5bd3269fde8d3f88bcce1131340f4c8
Matt Fischer <matt@mattfischer.com>
2016-12-05 22:46:26 +0000

    Support for mistral & mistral-newton config files
    Mistral is fun. Mainly because mistral itself is in
    upper-requirements.txt which causes issues when tox is run. Although
    mistral has a tox_install.sh script that should work around this, it
    does not due to pip path issues when the script is being run. This
    exception code instead just removes mistral from upper-requirements
    using grep -v and runs oslo_config_generator by hand. Like neutron
    mistral also has non-generated content which is handled in a post-copy
    step.
    
    As a part of this change I am fixing an indent/conditonal. Although
    we don't want to run tox specifially for mistral we also don't want to
    skip all the steps of prepping to copy files. Rather than duplicate
    this, I made the "run step" only do tox and nothing else.
    
    Change-Id: I451dc3200c030ee8dde92187cb166e4f24711393

--- support/update-configs.sh
+++ support/update-configs.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(cd "$(dirname "$0")"; pwd)
 EXCEPTIONDIR=$BASEDIR/exceptions
 
-PROJECTS="designate glance heat keystone nova neutron cinder ironic swift"
+PROJECTS="designate glance heat keystone mistral nova neutron cinder ironic swift"
 RELEASES="juno kilo liberty mitaka newton"
 
 declare -A BRANCHES
@@ -27,6 +27,7 @@ for PROJECT in $PROJECTS; do
     cd $BASEDIR/git-tmp
     git clean -f -x -d
     git checkout -f ${BRANCHES[$RELEASE]}
+    export RELEASE=${RELEASE}
 
     if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE}.pre ]; then
       . $EXCEPTIONDIR/${PROJECT}-${RELEASE}.pre
@@ -34,48 +35,48 @@ for PROJECT in $PROJECTS; do
       . $EXCEPTIONDIR/${PROJECT}.pre
     fi
 
-    if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE} ]; then
-      . $EXCEPTIONDIR/${PROJECT}-${RELEASE}
-    elif [ -f $EXCEPTIONDIR/${PROJECT} ]; then
-      . $EXCEPTIONDIR/${PROJECT}
+    if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE}.genconfig ]; then
+      . $EXCEPTIONDIR/${PROJECT}-${RELEASE}.genconfig
+    elif [ -f $EXCEPTIONDIR/${PROJECT}.genconfig ]; then
+      . $EXCEPTIONDIR/${PROJECT}.genconfig
     else
       if grep genconfig tox.ini; then
         # Work around bug in glance stable/kilo branch
         perl -i -pe 's!.*oslo-config-generator.*glance-search.conf.*$!!' tox.ini
         tox -r -e genconfig
       fi
-
-      if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE}.precopy ]; then
-        . $EXCEPTIONDIR/${PROJECT}-${RELEASE}.precopy
-      elif [ -f $EXCEPTIONDIR/${PROJECT}.precopy ]; then
-        . $EXCEPTIONDIR/${PROJECT}.precopy
-      fi
-
-      mkdir -p $BASEDIR/configs/$PROJECT/config/$RELEASE
-      if [ -d etc/$PROJECT ]; then
-        CONFDIR=etc/$PROJECT/
-      else
-        CONFDIR=etc/
-      fi
-
-      # Some of the sample nova config files are _sample.conf instead of
-      # .conf.sample which throws off the assumptions in the config file
-      # classes.
-      for i in $(find $CONFDIR -name '*_sample.conf'); do
-        mv $i $(dirname $i)/$(basename $i _sample.conf).conf.sample
-      done
-
-      # All of the sample swift config files are *.conf-sample instead of
-      # .conf.sample which throws off the assumptions in the config file
-      # classes.
-      for i in $(find $CONFDIR -name '*.conf-sample'); do
-        mv $i $(dirname $i)/$(basename $i .conf-sample).conf.sample
-      done
-
-      rm -rf $CONFDIR/oslo-config-generator
-      rsync -avP --delete --exclude 'README*.txt' --exclude 'README-*.conf*' --delete-excluded \
-        $CONFDIR $BASEDIR/configs/$PROJECT/config/$RELEASE/
     fi
+
+    if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE}.precopy ]; then
+      . $EXCEPTIONDIR/${PROJECT}-${RELEASE}.precopy
+    elif [ -f $EXCEPTIONDIR/${PROJECT}.precopy ]; then
+      . $EXCEPTIONDIR/${PROJECT}.precopy
+    fi
+
+    mkdir -p $BASEDIR/configs/$PROJECT/config/$RELEASE
+    if [ -d etc/$PROJECT ]; then
+      CONFDIR=etc/$PROJECT/
+    else
+      CONFDIR=etc/
+    fi
+
+    # Some of the sample nova config files are _sample.conf instead of
+    # .conf.sample which throws off the assumptions in the config file
+    # classes.
+    for i in $(find $CONFDIR -name '*_sample.conf'); do
+      mv $i $(dirname $i)/$(basename $i _sample.conf).conf.sample
+    done
+
+    # All of the sample swift config files are *.conf-sample instead of
+    # .conf.sample which throws off the assumptions in the config file
+    # classes.
+    for i in $(find $CONFDIR -name '*.conf-sample'); do
+      mv $i $(dirname $i)/$(basename $i .conf-sample).conf.sample
+    done
+
+    rm -rf $CONFDIR/oslo-config-generator
+    rsync -avP --delete --exclude 'README*.txt' --exclude 'README-*.conf*' --delete-excluded \
+      $CONFDIR $BASEDIR/configs/$PROJECT/config/$RELEASE/
 
     if [ -f $EXCEPTIONDIR/${PROJECT}-${RELEASE}.post ]; then
       . $EXCEPTIONDIR/${PROJECT}-${RELEASE}.post
