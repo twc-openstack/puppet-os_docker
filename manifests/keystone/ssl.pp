ee75267cb0a3de4955ee84b21aad26ca5ddd29c8
Tom Miller <C-Thomas.X.Miller@charter.com>
2017-01-30 08:33:54 -0700

    Keystone needs to mount certificates.
    In order for Keystone to start SSL endpoints the docker
    container will need to mount the SSL directory where
    the proper certificates are stored.
    
    Issue: CIRRUS-5907
    Change-Id: I42210a4edd9c300d2da15d959877791f6039166f

---
+++ manifests/keystone/ssl.pp
@@ -0,0 +1,61 @@
+# == Class: os_docker::keystone::ssl
+#
+# This class configures SSL for keystone docker images.
+#
+# === Parameters
+#
+# [*ssl_path*] (optional) Path where SSL certs will be installed.
+# Default: /etc/keystone/ssl
+#
+# [*cert*] (optional) Certificate for keystone endpoints. Default: undef
+#
+# [*key*] (optional) Keyfile for keystone endpoints. Default: undef
+#
+
+class os_docker::keystone::ssl(  
+  $ssl_path      = hiera('os_docker::keystone::ssl::ssl_path', '/etc/keystone/ssl'),
+  $cert          = hiera('os_docker::keystone::ssl::cert', undef),
+  $key           = hiera('os_docker::keystone::ssl::key', undef),
+)
+{
+  $cert_path = "${ssl_path}/certs"
+  $key_path = "${ssl_path}/private"
+
+  if $cert and $key {
+    File {
+      owner => 'keystone',
+      group => 'keystone',
+    }
+
+    file { $ssl_path:
+      ensure => 'directory',
+      mode   => '0755',
+    }
+
+    file { $cert_path:
+      ensure  => 'directory',
+      mode    => '0755',
+      require => File[$ssl_path],
+    }
+
+    file { $key_path:
+      ensure  => 'directory',
+      mode    => '0750',
+      require => File[$ssl_path],
+    }
+
+    file { "${cert_path}/keystone.pem":
+      ensure  => 'file',
+      content => $cert,
+      mode    => '0644',
+      require => File[$cert_path],
+    }
+
+    file { "${key_path}/keystonekey.pem":
+      ensure  => 'file',
+      content => $key,
+      mode    => '0640',
+      require => File[$key_path],
+    }
+  }
+}
